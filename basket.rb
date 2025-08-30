2b45bf60f56172267fc16964143361487c4673e6
binocodev <binocog@gmail.com>
2025-08-30 14:05:57 +0200

    CLEAN:rename vars for clarity

--- basket.rb
+++ basket.rb
@@ -4,15 +4,7 @@ require_relative 'delivery_calculator'
 require_relative 'offer_calculator'
 
 class Basket
-  attr_reader :catalog, :delivery_cost_rules, :offers, :order
-
-  def initialize(offers = OFFERS, catalog = CATALOG, delivery_cost_rules = DELIVERY_COST_RULES )
-    @catalog = catalog
-    @delivery_cost_rules = delivery_cost_rules
-    @offers = offers
-    @order = []
-    @offer_calculator = OfferCalculator.new(@catalog, @offers)
-  end
+  attr_reader :catalog, :offers, :order
 
   CATALOG = {
     'R01' => Product.new('R01', 32.95, 'Red Widget'),
@@ -20,46 +12,35 @@ class Basket
     'B01' => Product.new('B01', 7.95, 'Blue Widget')
   }.freeze
 
-  DELIVERY_COST_RULES = {
-    standard: 4.95,
-    discount: 2.95,
-    free: 0
-  }.freeze
-
   OFFERS = {
     red_widget_bogo: Offer.new('R01', 2, 0.5)
   }.freeze
 
+  def initialize(catalog = CATALOG, offers = OFFERS)
+    @catalog = catalog
+    @offers = offers
+    @order = []
+    @offer_calculator = OfferCalculator.new(@catalog, @offers)
+  end
+
   def add(product_code)
-    add_product(product_code)
-  end
-
-  def add_product(product_code)
     product = @catalog[product_code]
-    if product
-      @order << product
-      @offers_discount = nil
-    end
+    @order << product if product
   end
 
-  def add_products(product_codes)
-    product_codes.each { |code| add_product(code) }
+  def total
+    subtotal_after_discounts = subtotal - discount
+    delivery = DeliveryCalculator.calculate(subtotal_after_discounts)
+    (subtotal_after_discounts + delivery).round(2)
   end
 
-  def delivery_fee
-    subtotal_with_discounts = subtotal - offers_discount
-    DeliveryCalculator.calculate(subtotal_with_discounts)
-  end
+  private
 
   def subtotal
     @order.sum(&:price)
   end
 
-  def offers_discount
+  def discount
     @offer_calculator.calculate_discount(@order)
-  end
-
-  def total
-    (subtotal + delivery_fee - offers_discount).round(2)
   end
 end
