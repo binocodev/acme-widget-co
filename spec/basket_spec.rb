72bf51c2fef5ad8c9543fcc5a059d7b501668a87
binocodev <binocog@gmail.com>
2025-08-30 14:06:37 +0200

    FIX:fix specs for new interface

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -5,26 +5,33 @@ describe Basket do
   describe '#total' do
     it 'returns 37.85 for B01, G01' do
       basket = Basket.new
-      basket.add_products(['B01', 'G01'])
+      basket.add('B01')
+      basket.add('G01')
       expect(basket.total).to eq(37.85)
     end
 
-    it 'returns 54.38 for R01, R01' do
+    it 'returns 54.37 for R01, R01' do
       basket = Basket.new
-      basket.add_products(['R01', 'R01'])
-      expect(basket.total).to eq(54.38)
+      basket.add('R01')
+      basket.add('R01')
+      expect(basket.total).to eq(54.37)
     end
 
     it 'returns 60.85 for R01, G01' do
       basket = Basket.new
-      basket.add_products(['R01', 'G01'])
+      basket.add('R01')
+      basket.add('G01')
       expect(basket.total).to eq(60.85)
     end
 
-    it 'returns 98.28 for B01, B01, R01, R01, R01' do
+    it 'returns 98.27 for B01, B01, R01, R01, R01' do
       basket = Basket.new
-      basket.add_products(['B01', 'B01', 'R01', 'R01', 'R01'])
-      expect(basket.total).to eq(98.28)
+      basket.add('B01')
+      basket.add('B01')
+      basket.add('R01')
+      basket.add('R01')
+      basket.add('R01')
+      expect(basket.total).to eq(98.27)
     end
   end
 end
